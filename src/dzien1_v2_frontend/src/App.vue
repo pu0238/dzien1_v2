b0f2d2b21fa3077ae22734f1bf3cf451c1c1a226
pu0238 <pu0238@protonmail.com>
2024-06-25 22:06:33 +0200

    Added counter

--- src/dzien1_v2_frontend/src/App.vue
+++ src/dzien1_v2_frontend/src/App.vue
@@ -4,5 +4,22 @@
     <br />
     <br />
     hej blockchain!
+    {{ licznik }}
+    <button @click="podbij">Podbij</button>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      licznik: 0
+    }
+  },
+  methods: {
+    podbij() {
+      this.licznik++
+    }
+  }
+}
+</script>
